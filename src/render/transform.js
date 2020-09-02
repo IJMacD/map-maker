7063c1c62935fa088caa58b19e58e997f23215f9
Iain MacDonald <IJMacD@gmail.com>
2020-09-02 16:06:57 +0800

    Support multiple transforms
    matrix, rotate, scale, translate, skew

--- src/render/transform.js
+++ src/render/transform.js
@@ -6,10 +6,62 @@
  */
 export function applyTransform(ctx, rule) {
     if (rule.declarations["transform"]) {
-        const m = /matrix\(([-\d,.\s]*)\)/.exec(rule.declarations["transform"]);
-        if (m) {
-            const p = m[1].split(",");
-            ctx.transform(+p[0], +p[1], +p[2], +p[3], +p[4], +p[5]);
+        const r = /\s*([a-z]+)\(([^)]*)\)\s*/g;
+        const t = rule.declarations["transform"];
+        let m;
+        while (m = r.exec(t)) {
+            const trans = m[1];
+            const params = m[2].split(",").map(s => ({ value: parseFloat(s), unit: s.replace(/[-\d.\s]/g, "") }));
+            switch (trans) {
+                case "matrix":
+                    // @ts-ignore
+                    ctx.transform(...params.map(p => p.value));
+                    break;
+                case "rotate": {
+                    let { value, unit } = params[0];
+
+                    if (unit === "deg") {
+                        value *= Math.PI / 180;
+                    } else if (unit === "turn") {
+                        value *= Math.PI * 2;
+                    }
+
+                    ctx.rotate(value);
+                    break;
+                }
+                case "scale":
+                    const x = params[0].value;
+                    const y = params[1] ? params[1].value : x;
+                    ctx.scale(x, y);
+                    break;
+                case "translate":
+                    // @ts-ignore
+                    ctx.translate(...params.map(p => p.value));
+                    break;
+                case "skew":
+                    let { value, unit } = params[0];
+
+                    if (unit === "rad")
+                        value = Math.tan(value);
+                    else if (unit === "deg")
+                        value = Math.tan(value * Math.PI / 180);
+
+                    let valueY = 0;
+
+                    if (params[1]) {
+                        let { value, unit } = params[1];
+
+                        if (unit === "rad")
+                            value = Math.tan(value);
+                        else if (unit === "deg")
+                            value = Math.tan(value * Math.PI / 180);
+
+                        valueY = value;
+                    }
+
+                    ctx.transform(1, valueY, value, 1, 0, 0);
+                    break;
+            }
         }
     }
 }
