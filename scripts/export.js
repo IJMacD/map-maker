c1f9665f44d12872857075631028cd9ca3bfaf4b
Iain MacDonald <ijmacd@gmail.com>
2022-07-22 23:56:55 +0800

    Export can produce multiple zooms

--- scripts/export.js
+++ scripts/export.js
@@ -8,6 +8,7 @@ require("fetch-everywhere");
 const fs = require('fs');
 const { parseStyle, filterRules } = require("../src/Classes/Style");
 const { OverpassSource } = require("../src/ElementSources/OverpassSource");
+const { MemorySource } = require("../src/ElementSources/MemorySource");
 const { default: CanvasRender } = require("../src/render/CanvasRender");
 const { makeBBoxFromContext } = require("../src/util/bbox");
 const { mercatorProjection } = require('../src/util/util');
@@ -21,60 +22,92 @@ if (process.argv.length < 3) {
 // ElementSource is too noisy
 console.debug = () => void 0;
 
-const styleFilename = getArg("-s");
-// @ts-ignore
-// Arg is required so result cannot be null
-const style = fs.readFileSync(styleFilename, { encoding: "utf8" });
 
-if (!style) {
-    console.error(`Unable to load style from ${styleFilename}`);
-    process.exit();
+run();
+
+async function run() {
+    const styleFilename = getArg("-s");
+    // @ts-ignore
+    // Arg is required so result cannot be null
+    const style = fs.readFileSync(styleFilename, { encoding: "utf8" });
+
+    if (!style) {
+        console.error(`Unable to load style from ${styleFilename}`);
+        process.exit();
+    }
+
+    const boundsOption = getArg("-b");
+    const zoomOption = getArg("-z");
+    const centreOption = getArg("-c");
+    const widthOption = getArg("-w");
+    const heightOption = getArg("-h");
+
+    const scale = +(getArg("-x") ?? 1);
+
+    /** @type {MapContext[]} */
+    const contexts = [];
+
+    if (zoomOption) {
+        /** @type {number[]} */
+        const zooms = zoomOption.includes("-") ?
+            range.apply(null, zoomOption.split("-").map(v => +v)) :
+            [+zoomOption];
+
+        if (boundsOption) {
+            const bounds = /** @type {[number,number,number,number]} */(boundsOption.split(",").map(v => +v));
+
+            for (const zoom of zooms) {
+                contexts.push(makeContextFromBoundsZoom(bounds, zoom, scale));
+            }
+        }
+        else if (centreOption && widthOption && heightOption) {
+            const centre = /** @type {[number,number]} */(centreOption.split(",").map(v => +v));
+
+            for (let i = 0; i < zooms.length; i++) {
+                const zoom = zooms[i];
+                contexts.push(makeContextFromCentreZoomWidthHeight(centre, zoom, +widthOption * (2 ** i), +heightOption * (2 ** i), scale));
+            }
+        }
+    }
+    else if (boundsOption && widthOption && heightOption) {
+        // Possible but not implemented yet
+        throw Error("NotImplemented");
+    }
+
+    if (contexts.length === 0) {
+        console.error("The correct combination of -b, -z, -c, -w, and -h were not provided.");
+        process.exit();
+    }
+
+    /** @type {ElementSource} */
+    let elementSource = new OverpassSource();
+    if (contexts.length > 0) {
+        elementSource = new MemorySource(elementSource);
+    }
+
+    const styleRules = parseStyle(style);
+
+    const outFilename = getArg("-o") ?? "output.png";
+
+    for (const context of contexts) {
+
+        let outname = contexts.length > 1 ?
+            outFilename.replace(/\.[^.]+$/, s => `-${context.zoom}${s}`) :
+            outFilename;
+
+        console.log(context);
+
+        await createMap(styleRules, context, elementSource, outname);
+    }
 }
-
-const boundsOption = getArg("-b");
-const zoomOption = getArg("-z");
-const centreOption = getArg("-c");
-const widthOption = getArg("-w");
-const heightOption = getArg("-h");
-
-const scale = +(getArg("-x") ?? 1);
-
-/** @type {MapContext?} */
-let context = null;
-
-if (boundsOption && zoomOption) {
-    const bounds = /** @type {[number,number,number,number]} */(boundsOption.split(",").map(v => +v));
-    context = makeContextFromBoundsZoom(bounds, +zoomOption);
-}
-else if (centreOption && zoomOption && widthOption && heightOption) {
-    const centre = /** @type {[number,number]} */(centreOption.split(",").map(v => +v));
-    context = makeContextFromCentreZoomWidthHeight(centre, +zoomOption, +widthOption, +heightOption);
-}
-else if (boundsOption && widthOption && heightOption) {
-    // Possible but not implemented yet
-    throw Error("NotImplemented");
-}
-
-if (!context) {
-    console.error("The correct combination of -b, -z, -c, -w, and -h were not provided.");
-    process.exit();
-}
-
-const outFilename = getArg("-o") ?? "output.png";
-
-console.log(context);
-
-const styleRules = parseStyle(style);
-
-createMap(styleRules, context, outFilename);
-
 
 /**
  * @param {{ rules: (StyleRule|MediaQuery)[]; }} styleRules
  * @param {MapContext} context
+ * @param {ElementSource} elementSource
  * @param {string} outputFilename
  */
-async function createMap (styleRules, context, outputFilename) {
+async function createMap (styleRules, context, elementSource, outputFilename) {
 
     const filteredRules = filterRules(styleRules.rules, context);
 
@@ -89,12 +122,11 @@ async function createMap (styleRules, context, outputFilename) {
     // node canvas is a fine stand-in for HTMLCanvasElement
     const renderer = new CanvasRender(canvas);
 
-    await render(filteredRules, new OverpassSource(), renderer, context);
+    await render(filteredRules, elementSource, renderer, context);
 
     const out = fs.createWriteStream(outputFilename);
     const pngStream = canvas.createPNGStream();
     pngStream.pipe(out);
-    out.on("finish", () => console.log("Done"));
 }
 
 /**
@@ -134,7 +166,7 @@ function printUsage (stream = process.stdout) {
 Options:
     -s filename for style text
     -b bounds as minLon,minLat,maxLon,maxLat
-    -z zoom
+    -z zoom, if zoom is a range (e.g. 12-16) then multiple images will be produced
     -c centre as lon,lat
     -w image width
     -h image height
@@ -157,8 +189,9 @@ function getArg (flag) {
  * @param {number} width
  * @param {number} height
  * @returns {MapContext}
+ * @param {number} scale
  */
-function makeContextFromCentreZoomWidthHeight(centre, zoom, width, height) {
+function makeContextFromCentreZoomWidthHeight(centre, zoom, width, height, scale) {
     return { centre, zoom, width, height, scale };
 }
 
@@ -166,8 +199,9 @@ function makeContextFromCentreZoomWidthHeight(centre, zoom, width, height) {
  * @param {[number,number,number,number]} bounds
  * @param {number} zoom
  * @returns {MapContext}
+ * @param {number} scale
  */
- function makeContextFromBoundsZoom (bounds, zoom) {
+ function makeContextFromBoundsZoom (bounds, zoom, scale) {
     /** @type {[number,number]} */
     const centre = [(bounds[0] + bounds[2])/2, (bounds[1] + bounds[3])/2];
 
@@ -188,3 +222,11 @@ function makeContextFromCentreZoomWidthHeight(centre, zoom, width, height) {
     }
 }
 
+/**
+ * @param {number} start
+ * @param {number} end
+ */
+function range (start, end) {
+    const length = end - start + 1;
+    return Array.from({length}).map((_, i) => start + i);
+}
