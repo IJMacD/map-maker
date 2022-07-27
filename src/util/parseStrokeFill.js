ccf5b7ccd7e668f5afb8d0d85a8264e35a07b93b
Iain MacDonald <ijmacd@gmail.com>
2022-07-27 11:34:18 +0800

    Restore stroke shorthand
    e.g. 10px red;

--- src/util/parseStrokeFill.js
+++ src/util/parseStrokeFill.js
@@ -1,26 +1,44 @@
-import { evaluateColour } from "./evaluate";
+import { evaluateColour, evaluateDimension } from "./evaluate";
 
 /**
+ * Examples:
+ *  fill: red;
+ *  fill: #F00;
+ *  fill: rgba(255, 0, 0, 1);
+ *  fill: tag(colour);
+ *  fill: random;
+ *  stroke: blue;
+ *  stroke: 10px blue;
+ *  stroke-width: 15px;
+ *  stroke: 10 tag(building:colour);
+ *  stroke: 10 random;
+ *
  * @param {{ [property: string]: string }} declarations
  * @param {OverpassElement?} element
  * @param {MapContext} context
  */
 export function parseStrokeFill(declarations, element, context) {
-    const fillStyle = evaluateColour(declarations["fill"], element, context);
-    let lineWidth;
-    let strokeStyle = evaluateColour(declarations["stroke"], element, context);
-    /** @type {number[]} */
-    let lineDash;
-
     const { scale } = context;
 
-    if (declarations["stroke-width"]) {
-        lineWidth = +declarations["stroke-width"] * scale;
+    const fillStyle = evaluateColour(declarations["fill"], element, context);
+
+    /** @type {number} */
+    let lineWidth = 1 * scale;
+    let strokeColour = declarations["stroke"];
+    const dimenMatch = /^\s*\d+(?:\s*px)?/.exec(strokeColour);
+    if (dimenMatch) {
+        lineWidth = parseFloat(dimenMatch[0]);
+        strokeColour = strokeColour.substring(dimenMatch[0].length).trim();
     }
-    else {
-        lineWidth = scale;
+    let strokeStyle = evaluateColour(strokeColour, element, context);
+
+
+    if (declarations["stroke-width"]) {
+        lineWidth = +evaluateDimension(declarations["stroke-width"], element, context) * scale;
     }
 
+    /** @type {number[]} */
+    let lineDash;
     if (declarations["stroke-dash"]) {
         lineDash = declarations["stroke-dash"].split(" ").map(s => +s * scale);
     }
